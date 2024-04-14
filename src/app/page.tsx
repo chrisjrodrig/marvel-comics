a6f193357541b45e08d2a6c89c6f992a42111919
chrisjrodrig <chris.j.rodrig@gmail.com>
2024-04-14 07:59:19 -0700

    added links on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Homepage
+      <a href="/comics">All Comics</a>
+      <a href="/series">All Series</a>
     </main>
   );
 }
